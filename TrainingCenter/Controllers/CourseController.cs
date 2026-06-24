07e1529394d28fa9e917e79fb7bfb59cce48cb0b
Mohamed Samy <msamyaly17@gmail.com>
2026-06-24 15:05:45 +0300

    update Day2

---
+++ TrainingCenter/Controllers/CourseController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TrainingCenter.Models;
+
+namespace TrainingCenter.Controllers
+{
+    public class CourseController : Controller
+    {
+        TrainingCenterContext context = new TrainingCenterContext();
+        public IActionResult Index()
+        {
+            List<Course> courses = context.Courses.Include(d=> d.Department).ToList();
+            return View("Index", courses);
+        }
+    }
+}
--- TrainingCenter/Controllers/InstructoreController.cs
+++ TrainingCenter/Controllers/InstructoreController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using TrainingCenter.Models;
 using TrainingCenter.ViewModel.Instructore;
 
@@ -8,44 +9,53 @@ namespace TrainingCenter.Controllers
     {
         TrainingCenterContext context = new TrainingCenterContext();
 
-        List<InstructoreWithListOfDepartAndCourseVM> getIstructorsData()
-        {
-            List<Instructore> instructors = context.Instructors.ToList();
-            List<Department> departments = context.Departments.ToList();
-            List<Course> courses = context.Courses.ToList();
+        //List<InstructoreWithListOfDepartAndCourseVM> getIstructorsData()
+        //{
+        //    List<Instructore> instructors = context.Instructors.ToList();
+        //    List<Department> departments = context.Departments.ToList();
+        //    List<Course> courses = context.Courses.ToList();
 
-            List<InstructoreWithListOfDepartAndCourseVM> instrcturesForView = new();
+        //    List<InstructoreWithListOfDepartAndCourseVM> instrcturesForView = new();
 
-            foreach (Instructore item in instructors)
-            {
-                instrcturesForView
-                    .Add(new InstructoreWithListOfDepartAndCourseVM()
-                    {
-                        Id = item.Id,
-                        Name = item.Name,
-                        Salary = item.Salary,
-                        Address = item.Address,
-                        ImageUrl = item.ImageUrl,
-                        DeptId = item.DeptId,
-                        DeptName = departments.FirstOrDefault
-                                    (d => d.Id == item.DeptId).Name,
-                        CrsId = item.CrsId,
-                        CrsName = courses.FirstOrDefault(c => c.Id == item.CrsId).Name
-                    });
-            }
-            return instrcturesForView;
-        }
+        //    foreach (Instructore item in instructors)
+        //    {
+        //        instrcturesForView
+        //            .Add(new InstructoreWithListOfDepartAndCourseVM()
+        //            {
+        //                Id = item.Id,
+        //                Name = item.Name,
+        //                Salary = item.Salary,
+        //                Address = item.Address,
+        //                ImageUrl = item.ImageUrl,
+        //                DeptId = item.DeptId,
+        //                DeptName = departments.FirstOrDefault
+        //                            (d => d.Id == item.DeptId).Name,
+        //                CrsId = item.CrsId,
+        //                CrsName = courses.FirstOrDefault(c => c.Id == item.CrsId).Name
+        //            });
+        //    }
+        //    return instrcturesForView;
+        //}
 
 
         public IActionResult Index()
         {
-            return View("Index", getIstructorsData());
+            //List<Instructore> instructores = context.Instructors
+            //        .Include(d => d.Department)
+            //        .Include(c => c.Course)
+            //        .ToList();
 
+            return View("Index", context.Instructors
+                    .Include(d => d.Department)
+                    .Include(c => c.Course)
+                    .ToList());
         }
-        public IActionResult Details (int id)
+        public IActionResult Details(int id)
         {
             return View("Details",
-                getIstructorsData().FirstOrDefault(i => i.Id == id));
+                context.Instructors.Include(d => d.Department)
+                .Include(c => c.Course)
+                .FirstOrDefault(i => i.Id == id));
         }
     }
 }
